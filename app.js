34590d54089ed6c41fbb39aedfe86f67fad75692
Aryan <chaudharyaryan3456@gmail.com>
2024-11-12 21:53:40 +0530

    completed practiceqs

---
+++ app.js
@@ -0,0 +1,32 @@
+// 1. Trim Method 
+// let msg = "   hel  lo   ";
+
+// let password = prompt("Set your password");
+// console.log(password.trim());
+
+// 2. toUpperCase() and toLowerCase() Methods 
+// let name = "Aryan";
+// let msg = "error";
+// console.log(msg.toUpperCase());
+
+// 3. indexOf Method 
+// let str = "IloveCoding";
+// console.log(str.indexOf("love"));
+// console.log(str.indexOf("J"));
+// console.log(str.indexOf("o"));
+
+// Method chaining 
+// let msg = "   hello   ";
+// let newMsg = msg.trim();
+// console.log("after trim :", newMsg)
+// newMsg = newMsg.toUpperCase();
+// console.log("after Uppercase :", newMsg);
+// let newMsg = msg.trim().toUpperCase();
+// console.log(newMsg);
+
+// 4. slice() method 
+// let msg = "Aryantanwar";
+// console.log(msg.slice(-5));
+
+// 5. replace() and repeat() method 
+let msg = "IloveCoding";
---
+++ practiceqs.js
@@ -0,0 +1,12 @@
+// Question No. 1 - For the given String : 
+// Trim it & convert it to uppercase. 
+
+let msg = "!help";
+console.log(msg.trim().toUpperCase());
+
+// Question No. 2 - For the String -> let name = "ApnaCollege", predict the output for following: 
+
+let name = "ApnaCollege";
+console.log(name.slice(4, 9));     //Colle
+console.log(name.indexOf("na"));   //2
+console.log(name.replace("Apna", "Our"));   //OurCollege 
