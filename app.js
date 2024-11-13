543518338985bfd0f06a47bbd6cd5648dbb235ee
Aryan <chaudharyaryan3456@gmail.com>
2024-11-13 22:56:33 +0530

    updated

--- app.js
+++ app.js
@@ -29,4 +29,26 @@
 // console.log(msg.slice(-5));
 
 // 5. replace() and repeat() method 
-let msg = "IloveCoding";
+// let msg = "IloveCoding";
+
+// 6. Array (data Structure & visualizing Array)
+// let marks = [99, 89, 67, 42, 100];
+// let names = ["Aryan", "Tanwar", "chaudhary"];
+// let info = ["Aryan", 89, 10.2];
+// console.log(info[0].length);
+
+// 7. Array Methods 
+let cars = ["Audi", "bmw", "xuv", "Maruti"];
+// console.log(cars.push("toyota"));  //add element to the last of the array.
+// console.log(cars.pop());  //deletes an element from the last of the array.
+// console.log(cars.shift());  //deletes an element from the starting of the array.
+// console.log(cars.unshift("toyota"));  //adds an element to the starting of the array.
+// console.log(cars.indexOf("toyota"));  //returns the index of the particular character written within the parenthesis.
+// console.log(cars.includes("toyota"));  //searches for a value inside the array and returns true or false depending upon the existence of the particular elements.
+// console.log(cars.reverse());  //reverse the elements of the original array and returns the original array as reversed. 
+// console.log(cars.slice());  //returns the extracted part of an array.
+console.log(cars.splice());  //changes the original array.
+console.log(cars.sort());  //arranging elements in ascending or descending manner.
+
+
+
--- practiceqs.js
+++ practiceqs.js
@@ -1,12 +1,24 @@
 // Question No. 1 - For the given String : 
 // Trim it & convert it to uppercase. 
 
-let msg = "!help";
-console.log(msg.trim().toUpperCase());
+// let msg = "!help";
+// console.log(msg.trim().toUpperCase());
 
 // Question No. 2 - For the String -> let name = "ApnaCollege", predict the output for following: 
 
-let name = "ApnaCollege";
-console.log(name.slice(4, 9));     //Colle
-console.log(name.indexOf("na"));   //2
-console.log(name.replace("Apna", "Our"));   //OurCollege 
+// let name = "ApnaCollege";
+// console.log(name.slice(4, 9));     //Colle
+// console.log(name.indexOf("na"));   //2
+// console.log(name.replace("Apna", "Our"));   //OurCollege 
+
+//Question No. 3 - For the given start state of an array, change it to final form using methods.
+// start : ["january", "july", "march", "august"];
+// final : ["july", "june", "march", "august"];
+
+let start = ["january", "july", "march", "august"];
+start.shift();
+start.shift();
+start.unshift("june");
+start.unshift("july");
+console.log(start);
+
